c851f4d868cb7757591f0525d38ebc697b4fac02
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-08 23:54:28 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+ ogr2ogr data.parquet "/vsicurl/https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/USGS_Seismic_Data_v1/FeatureServer/0/query?f=geojson&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=mag%20DESC&outSR=3857&resultOffset=0&resultRecordCount=10000"
