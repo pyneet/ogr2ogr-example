451decafc1a9adca8c51efaa9afa6fa387f75bcb
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 11:16:49 -0400

    Update run.sh to change CRS to EPSG:4326 / WGS84

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-curl --output data.geojson "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/USGS_Seismic_Data_v1/FeatureServer/0/query?f=geojson&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=mag%20DESC&outSR=3857&resultOffset=0&resultRecordCount=10000"
+curl --output data.geojson "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/USGS_Seismic_Data_v1/FeatureServer/0/query?f=geojson&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=mag%20DESC&outSR=4326&resultOffset=0&resultRecordCount=10000"
 
 ogr2ogr data.parquet data.geojson
