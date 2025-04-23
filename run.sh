ae5074f1ba6c52a731d4e0f93fab3b7face6aaa0
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 11:30:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-curl --output data.geojson "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Transportation_Rail_Bus_WebMercator/MapServer/58/query?outFields=*&where=1%3D1&f=geojson"
+curl --output data_dc_metro.geojson "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Transportation_Rail_Bus_WebMercator/MapServer/58/query?outFields=*&where=1%3D1&f=geojson"
 
 ogr2ogr data_dc_metro.parquet data_dc_metro.geojson
